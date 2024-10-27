9d2dcabdabb88fc501ef5a52d61d312ed1696b1b
Ademitidayo <adenijibimmy08@gmail.com>
2024-10-27 23:38:58 +0100

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,7 +7,7 @@ def canUnlockAll(boxes):
     setofkeys = [0]
     counter = 0
     index = 0
-
+    
     while index < len(setofkeys):
         setkey = setofkeys[index]
         for key in boxes[setkey]:
@@ -15,5 +15,4 @@ def canUnlockAll(boxes):
                 setofkeys.append(key)
                 counter += 1
         index += 1
-
-    return counter == total_boxes - 1
+        return counter == total_boxes - 1
