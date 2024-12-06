7096e3dde6744ea02fdc287d5d5b8a2fcd4890c5
Ademitidayo <adenijibimmy08@gmail.com>
2024-12-06 02:02:28 +0100

    perim

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,22 +7,18 @@ def island_perimeter(grid):
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 1:
-                # look up
                 if y == 0:
                     count += 1
                 elif grid[y-1][x] == 0:
                     count += 1
-                # look down
                 if y == (len(grid) - 1):
                     count += 1
                 elif grid[y+1][x] == 0:
                     count += 1
-                # look left
                 if x == 0:
                     count += 1
                 elif grid[y][x-1] == 0:
                     count += 1
-                # look right
                 if x == (len(grid[y]) - 1):
                     count += 1
                 elif grid[y][x+1] == 0:
